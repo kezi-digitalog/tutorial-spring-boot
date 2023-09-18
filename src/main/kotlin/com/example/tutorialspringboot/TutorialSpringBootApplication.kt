e6b62905593e83704647a085a3038a29ee6c9b8e
designkezi <02racoonz@gmail.com>
2023-09-18 23:17:57 +0900

    init project

---
+++ src/main/kotlin/com/example/tutorialspringboot/TutorialSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tutorialspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutorialSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<TutorialSpringBootApplication>(*args)
+}
