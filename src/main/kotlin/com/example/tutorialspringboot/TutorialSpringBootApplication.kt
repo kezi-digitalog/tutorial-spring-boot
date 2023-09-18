10f9d58b230dce7d6417b231a916510d6c92cff5
designkezi <02racoonz@gmail.com>
2023-09-18 23:32:36 +0900

    add controller

--- src/main/kotlin/com/example/tutorialspringboot/TutorialSpringBootApplication.kt
+++ src/main/kotlin/com/example/tutorialspringboot/TutorialSpringBootApplication.kt
@@ -2,9 +2,18 @@ package com.example.tutorialspringboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
 
 @SpringBootApplication
 class TutorialSpringBootApplication
+
+@Controller
+class FirstController {
+    @RequestMapping(value = ["/user"], method = arrayOf(RequestMethod.GET))
+    @ResponseBody
+    fun hello() = "hello world"
+}
 
 fun main(args: Array<String>) {
     runApplication<TutorialSpringBootApplication>(*args)
