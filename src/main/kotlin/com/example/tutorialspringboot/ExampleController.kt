efe789f0653fe185cd919c32b09a53a0d2e45196
designkezi <02racoonz@gmail.com>
2023-09-20 23:37:24 +0900

    ConcurrentHashMap test

--- src/main/kotlin/com/example/tutorialspringboot/ExampleController.kt
+++ src/main/kotlin/com/example/tutorialspringboot/ExampleController.kt
@@ -3,18 +3,22 @@ package com.example.tutorialspringboot
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
+import java.util.concurrent.ConcurrentHashMap
 
 @Controller
 class ExampleController() {
     @Autowired
     lateinit var service: ServiceInterface
 
+    @Autowired
+    lateinit var customers: ConcurrentHashMap<Int, Customer>
+
     @RequestMapping(value = ["/user/{name}"], method = arrayOf(RequestMethod.GET))
     @ResponseBody
-    fun hello(@PathVariable name: String): Example {
+    fun hello(@PathVariable name: String): Customer? {
         println(service.getHello(name))
-        return Example(123, "test")
+        return customers[4]
     }
 }
 
-data class Example(var id: Int = 0, var name: String= "")
+data class Customer(var id: Int = 0, var name: String= "")
--- src/main/kotlin/com/example/tutorialspringboot/TutorialSpringBootApplication.kt
+++ src/main/kotlin/com/example/tutorialspringboot/TutorialSpringBootApplication.kt
@@ -2,10 +2,24 @@ package com.example.tutorialspringboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-class TutorialSpringBootApplication
+class TutorialSpringBootApplication{
+    companion object {
+        val initialCustomers = arrayOf(
+            Customer(1, "Kotlin"),
+            Customer(2, "Spring"),
+            Customer(3, "Microservice")
+        )
+    }
+
+    @Bean
+    fun customers() = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
+}
 
 fun main(args: Array<String>) {
     runApplication<TutorialSpringBootApplication>(*args)
 }
+
